95461d473dd8f637a08b104838470ee4c4848a8c
John Morrow <jmorrow@amoeba.org>
2026-03-14 22:03:19 -0700

    Update SHA256 for public repo release

--- Formula/zestful.rb
+++ Formula/zestful.rb
@@ -3,7 +3,7 @@ class Zestful < Formula
   homepage "https://zestful.dev"
   url "https://github.com/caladriuslogic/zestful/releases/download/v1.0.0/zestful"
   version "1.0.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "b62714f01dffdaccd8248e294b41c1c2156dccc80b6df91ff1796efacd914118"
   license "MIT"
 
   def install
